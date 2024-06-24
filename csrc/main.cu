3c63c0f963d3281fd32409999e5c22ecbd6662eb
Osayamen Aimuyo <oja7@cornell.edu>
2024-06-24 03:05:42 +0000

    add NVTX

--- csrc/main.cu
+++ csrc/main.cu
@@ -5,6 +5,7 @@
 #include <cuda/std/chrono>
 #include <cute/util/debug.hpp>
 #include <nvshmemx.h>
+#include <nvtx3/nvtx3.hpp>
 
 #define HANDLE_CUDA_ERROR(x)                                      \
 { const auto err = x;                                             \
@@ -29,28 +30,35 @@ __global__ void aristos(int *destination, int my_pe, int n_pes, bool skip = true
     }
 }
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    int my_pe_node;
-    std::array<int, 4> msg{};
-    cudaStream_t stream;
+    {
+        NVTX3_FUNC_RANGE()
+        std::cout << "Hello, World!" << std::endl;
+        int my_pe_node;
+        std::array<int, 4> msg{};
+        cudaStream_t stream;
 
-    nvshmem_init();
-    my_pe_node = nvshmem_team_my_pe(NVSHMEMX_TEAM_NODE);
-    cudaSetDevice(my_pe_node);
-    cudaStreamCreate(&stream);
+        nvshmem_init();
+        my_pe_node = nvshmem_team_my_pe(NVSHMEMX_TEAM_NODE);
+        cudaSetDevice(my_pe_node);
+        cudaStreamCreate(&stream);
 
-    int *destination = (int *)nvshmem_calloc(4, sizeof(int));
-    for(int i = 0; i < 20; ++i){
-        aristos<<<1, 1, 0, stream>>>(destination, nvshmem_my_pe(), nvshmem_n_pes());
+        int *destination = (int *) nvshmem_calloc(4, sizeof(int));
+        for (int i = 0; i < 20; ++i) {
+            nvtx3::scoped_range test_run("Aristos Test Run");
+            aristos<<<1, 1, 0, stream>>>(destination, nvshmem_my_pe(), nvshmem_n_pes());
+        }
+        {
+            nvtx3::scoped_range range("Aristos");
+            aristos<<<1, THREADS_PER_WARP, 0, stream>>>(destination, nvshmem_my_pe(), nvshmem_n_pes(), false);
+        }
+        nvshmemx_barrier_all_on_stream(stream);
+        HANDLE_CUDA_ERROR(cudaMemcpyAsync(msg.data(), destination, 4 * sizeof(int), cudaMemcpyDeviceToHost, stream))
+        cudaStreamSynchronize(stream);
+        std::cout << "PE " << nvshmem_my_pe()
+                  << " received message " << msg[2] << std::endl;
+
+        nvshmem_free(destination);
+        nvshmem_finalize();
     }
-    aristos<<<1, THREADS_PER_WARP, 0, stream>>>(destination, nvshmem_my_pe(), nvshmem_n_pes(), false);
-    nvshmemx_barrier_all_on_stream(stream);
-    HANDLE_CUDA_ERROR(cudaMemcpyAsync(msg.data(), destination, 4 * sizeof(int), cudaMemcpyDeviceToHost, stream))
-    cudaStreamSynchronize(stream);
-    std::cout << "PE " << nvshmem_my_pe()
-              << " received message " << msg[2] << std::endl;
-
-    nvshmem_free(destination);
-    nvshmem_finalize();
     return 0;
 }
