13fd2e17543240af8637ae3a8b02cc34737559b3
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2025-01-04 23:29:02 -0500

    lint

--- csrc/include/indexing.cuh
+++ csrc/include/indexing.cuh
@@ -5,66 +5,44 @@
 #ifndef ARISTOS_INDEXING_CUH
 #define ARISTOS_INDEXING_CUH
 
-#include <../cmake/cache/cutlass/3a9de332b38746b489cce3a24161f80aafa1feaa/include/cute/config.hpp>
-
 namespace aristos{
     namespace block{
         /// Block-scoped thread id
         decltype(auto)
-        CUTE_HOST_DEVICE
+        __device__ __forceinline__
         threadID() {
-            #if defined(__CUDA_ARCH__)
-                        return threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y;
-            #else
-                        return 0;
-            #endif
+            return threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y;
         }
 
         /// Block-scoped warp id
         decltype(auto)
-        CUTE_HOST_DEVICE
+        __device__ __forceinline__
         warpID() {
-            #if defined(__CUDA_ARCH__)
-                        return (threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y) >> 5;
-            #else
-                        return 0;
-            #endif
+            return (threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y) >> 5;
         }
     }
     namespace warp{
         /// Block-scoped and warp-scoped thread id
         decltype(auto)
-        CUTE_HOST_DEVICE
+        __device__ __forceinline__
         laneID() {
-        #if defined(__CUDA_ARCH__)
-                    return (threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y) % 32;
-        #else
-                    return 0;
-        #endif
+            return (threadIdx.x + threadIdx.y*blockDim.x + threadIdx.z*blockDim.x*blockDim.y) % 32;
         }
     }
     namespace grid{
         /// Grid-scoped thread id
         decltype(auto)
-        CUTE_HOST_DEVICE
+        __device__ __forceinline__
         threadID() {
-        #if defined(__CUDA_ARCH__)
             return (blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z)
-                    * (blockDim.x * blockDim.y * blockDim.z)
-                    + (threadIdx.z * (blockDim.x * blockDim.y)) + (threadIdx.y * blockDim.x) + threadIdx.x;
-        #else
-                    return 0;
-        #endif
+                        * (blockDim.x * blockDim.y * blockDim.z)
+                        + (threadIdx.z * (blockDim.x * blockDim.y)) + (threadIdx.y * blockDim.x) + threadIdx.x;
         }
 
         decltype(auto)
-        CUTE_HOST_DEVICE
+        __device__ __forceinline__
         blockID() {
-        #if defined(__CUDA_ARCH__)
-                    return blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
-        #else
-                    return 0;
-        #endif
+            return blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
         }
     }
 }
