fa378139d9387f9a8008f8bd9e1c91017680b945
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2025-05-13 19:41:32 -0400

    Clean Debug

--- csrc/include/debug.cuh
+++ csrc/include/debug.cuh
@@ -9,28 +9,29 @@
 #include <typeinfo>
 #include <cxxabi.h>
 #if !defined(CHECK_ERROR_EXIT)
-#  define CHECK_ERROR_EXIT(e)                                         \
-do {                                                           \
-cudaError_t code = (e);                                      \
-if (code != cudaSuccess) {                                   \
-fprintf(stderr, "<%s:%d> %s:\n    %s: %s\n",               \
-__FILE__, __LINE__, #e,                            \
-cudaGetErrorName(code), cudaGetErrorString(code)); \
-fflush(stderr);                                            \
-exit(1);                                                   \
-}                                                            \
+#  define CHECK_ERROR_EXIT(e)                                \
+do {                                                         \
+    cudaError_t code = (e);                                  \
+    if (code != cudaSuccess) {                               \
+        fprintf(stderr, "<%s:%d> %s:\n    %s: %s\n",         \
+            __FILE__, __LINE__, #e,                          \
+            cudaGetErrorName(code),                          \
+            cudaGetErrorString(code));                       \
+        fflush(stderr);                                      \
+        exit(1);                                             \
+    }                                                        \
 } while (0)
 #endif
 
-#define ARISTOS_CHECK_PREDICATE(predicate, errmsg)                  \
-do {                                                                \
-    if ((predicate) != 0) {                                         \
-        std::cerr   << "Error: " errmsg                             \
-                    << " [" << __PRETTY_FUNCTION__                  \
-                    << __FILE__ << ":" << __LINE__ << "]"           \
-                    << std::endl;                                   \
-        std::quick_exit(EXIT_FAILURE);                              \
-    }                                                               \
+#define ARISTOS_CHECK_PREDICATE(predicate, errmsg)           \
+do {                                                         \
+    if (!(predicate)) {                                      \
+        std::cerr   << "Error: " errmsg                      \
+                    << " [" << __PRETTY_FUNCTION__           \
+                    << __FILE__ << ":" << __LINE__ << "]"    \
+                    << std::endl;                            \
+        std::quick_exit(EXIT_FAILURE);                       \
+    }                                                        \
 } while (0)
 
 
