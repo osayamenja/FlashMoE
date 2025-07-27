73b7136e472a2faf23069e12379fe40c8cb50f7b
Osayamen Jonathan Aimuyo <oja7@cornell.edu>
2025-07-27 05:16:11 +0000

    Add MainLoop

--- csrc/include/kleos/os/processor/gemm.cuh
+++ csrc/include/kleos/os/processor/gemm.cuh
@@ -12,6 +12,7 @@
 #ifndef GEMM_CUH
 #define GEMM_CUH
 
+#include <cublasdx.hpp>
 #include <cutlass/epilogue/thread/activation.h>
 #include <cutlass/gemm/collective/collective_mma.hpp>
 
@@ -55,6 +56,91 @@ namespace kleos {
     template<typename Element, typename ActivationFunction>
     struct isFAA<FAA<Element, ActivationFunction>> : cuda::std::true_type {};
 
+    template<int N>
+    __device__ __forceinline__
+    void cpWait() {
+        cute::cp_async_wait<N>();
+        __syncthreads();
+    }
+    // GEMM Mainloop like that of CUTLASS
+    template<
+        typename BLAS,
+        int N,
+        int K,
+        int pipeStages,
+        typename Accumulator
+    >
+    requires(cutlass::is_pow2<pipeStages>::value)
+    struct GM {
+        __forceinline__ __device__
+        void operator()(void* __restrict__ const& workspace,
+            Accumulator& accumulator,
+            const typename BLAS::a_value_type* __restrict__ const& a,
+            const typename BLAS::b_value_type* __restrict__ const& b,
+            typename BLAS::a_value_type* __restrict__ const& c,
+            const int& M, const int& tileIdx) const {
+            using BM = cute::Int<cublasdx::size_of<BLAS>::m>;
+            using BN = cute::Int<cublasdx::size_of<BLAS>::n>;
+            using BK = cute::Int<cublasdx::size_of<BLAS>::k>;
+            using BT = cute::Shape<BM, BN, BK>;
+
+            const auto tilesM = M / BM{};
+            using KT = cute::Int<K>;
+            using NT = cute::Int<N>;
+            using tilesN = cute::Int<(N / BN{})>;
+            using tilesK = cute::Int<(K / BK{})>;
+
+            const auto tileCoord = cute::idx2crd(tileIdx, cute::Shape(tilesM, tilesN{}),
+                cute::Stride<cute::Int<tilesN{}>, cute::_1>{});
+            const auto ctaCoord = cute::make_coord(cute::get<0>(tileCoord), cute::get<1>(tileCoord), cute::_);
+            const auto strideA = cute::conditional_return<cublasdx::arrangement_of_v_a<BLAS> == cublasdx::row_major>
+            (cute::Stride<KT, cute::_1>{}, cute::make_stride(cute::_1{}, M));
+            const auto mA = cute::make_tensor(cute::make_gmem_ptr(a),
+                cute::make_layout(cute::make_shape(M, KT{}), strideA));
+            using StrideB = cuda::std::conditional_t<cublasdx::arrangement_of_v_b<BLAS> == cublasdx::row_major,
+            cute::Stride<NT, cute::_1>, cute::Stride<cute::_1, KT>>;
+            using ShapeB = cute::Shape<KT, NT>;
+            const auto mB = cute::make_tensor(cute::make_gmem_ptr(a),
+                cute::Layout<ShapeB, StrideB>{});
+            const auto strideC = cute::conditional_return<cublasdx::arrangement_of_v_c<BLAS> == cublasdx::row_major>
+            (cute::Stride<NT, cute::_1>{}, cute::make_stride(cute::_1{}, M));
+            const auto mC = make_tensor(cute::make_gmem_ptr(c),
+            make_layout(cute::make_shape(M, N), strideC));
+
+            const auto gA = cute::local_tile(mA, BT{}, ctaCoord, cute::Step<cute::_1, cute::X,cute::_1>{});
+            const auto gB = cute::local_tile(mB, BT{}, ctaCoord, cute::Step<cute::_1, cute::X,cute::_1>{});
+            const auto gC = cute::local_tile(mC, BT{}, ctaCoord, cute::Step<cute::_1, cute::X,cute::_1>{});
+
+            // shared layouts
+            constexpr auto sALay = cute::tile_to_shape(BLAS::suggest_layout_smem_a().layout,
+                cute::Shape<BM, BK, cute::Int<pipeStages>>{});
+            constexpr auto sBLay = cute::tile_to_shape(BLAS::suggest_layout_smem_b().layout,
+                cute::Shape<BK, BN, cute::Int<pipeStages>>{});
+            const auto [sA, sB] = cublasdx::shared_memory::slice<typename BLAS::a_value_type, typename BLAS::b_value_type>(
+                workspace, cublasdx::alignment_of_v_a<BLAS>, sALay, cublasdx::alignment_of_v_b<BLAS>, sBLay);
+            cute::for_each(cute::make_int_sequence<pipeStages>{}, [&](auto stage) {
+                cublasdx::copy<BLAS, cublasdx::alignment_of_v_a<BLAS>>(gA(cute::_, cute::_, stage), sA(cute::_, cute::_, stage));
+                cublasdx::copy<BLAS, cublasdx::alignment_of_v_b<BLAS>>(gB(cute::_, cute::_, stage), sB(cute::_, cute::_, stage));
+                cute::cp_async_fence();
+            });
+
+            cute::for_each(cute::make_int_range<pipeStages, tilesK{}>{}, [&](auto kStage) {
+                constexpr auto ps = cute::Int<kStage % pipeStages>{};
+                cpWait<pipeStages - 1>();
+                BLAS().execute(sA(cute::_, cute::_, ps), sB(cute::_, cute::_, ps), accumulator);
+                __syncthreads();
+                cublasdx::copy<BLAS, cublasdx::alignment_of_v_a<BLAS>>(gA(cute::_, cute::_, kStage), sA(cute::_, cute::_, ps));
+                cublasdx::copy<BLAS, cublasdx::alignment_of_v_b<BLAS>>(gB(cute::_, cute::_, kStage), sB(cute::_, cute::_, ps));
+                cute::cp_async_fence();
+            });
+
+            cute::for_each(cute::make_int_rsequence<pipeStages>{}, [&](auto rStage) {
+                constexpr auto ps = cute::Int<((tilesK{} - (rStage + 1)) % pipeStages)>{};
+                cpWait<rStage>();
+                BLAS().execute(sA(cute::_, cute::_, ps), sB(cute::_, cute::_, ps), accumulator);
+            });
+        }
+    };
     template<
         typename ActivationOp,
         typename ElementA,
